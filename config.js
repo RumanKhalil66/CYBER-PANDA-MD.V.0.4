3876fc23dc3a4ee2af6005762accec0794109d0b
RumanKhalil <150658812+RumanKhalil66@users.noreply.github.com>
2024-03-26 05:51:11 +0500

    Update config.js

--- config.js
+++ config.js
@@ -21,10 +21,10 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94704274987", "+94704274987"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94704274987", "+94704274987"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94704274987"];
-global.OwnerName = "SACHITH";
+global.Owner = ["+923400738466"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+923400738466"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+923400738466"];
+global.OwnerName = "RumanKhalil";
 global.BotName = "SACHITH";
 global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
 global.author = "SACHITH";                               //Do not change.
